738c2fc000fb1d2f9267ef0078483e9070f3dfa0
lelis <lelis685@gmail.com>
2025-01-18 19:47:13 -0300

    refatorando

--- src/main/java/com/br/service/PrecificadorService.java
+++ src/main/java/com/br/service/PrecificadorService.java
@@ -8,7 +8,6 @@ import com.br.dto.api.TipoCorteDataResponseApi;
 import com.google.common.collect.Iterables;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,43 +17,42 @@ public class PrecificadorService {
 
     public MessageCompletaDTO precificar(RequestApi requestApi) {
         long startTime = System.nanoTime();
-
         ApiService apiService = new ApiService();
 
-        Map<Integer, Integer> cortesResponseMap = new HashMap<>();
+        List<TipoCorteDataResponseApi> result = new ArrayList<>();
 
-        Iterables.partition(requestApi.getCortes(), 2)
+        Iterables.partition(requestApi.getCortes(), 100)
                 .forEach(subListDeCortes -> {
-                    apiService.callApi(requestApi, subListDeCortes).getCortesPrecos()
-                            .forEach(corte -> cortesResponseMap.put(corte.getId(), corte.getPreco()));
-                });
+                            List<TipoCorteDataResponseApi> cortesComPrecos = apiService.callApi(requestApi, subListDeCortes).getCortesPrecos();
+                            result.addAll(cortesComPrecos);
+                        }
+                );
 
-        List<TipoCorteDataCompleto> corteDataCompletos = requestApi.getCortes()
-                .stream()
-                .map(corte -> {
-                    Integer preco = cortesResponseMap.get(corte.getId());
-                    return TipoCorteDataCompleto.builder()
-                            .id(corte.getId())
-                            .tipoCorte(corte.getTipoCorte())
-                            .barbeiro(corte.getBarbeiro())
-                            .preco(preco)
-                            .build();
-                }).toList();
+        Map<Integer, Integer> cortesResponseMap = result.stream()
+                .collect(Collectors.toMap(TipoCorteDataResponseApi::getId, TipoCorteDataResponseApi::getPreco));
 
-        MessageCompletaDTO result = MessageCompletaDTO.builder()
-                .cnpj(requestApi.getCnpj())
-                .nome(requestApi.getNome())
-                .cortes(corteDataCompletos)
-                .build();
+        List<TipoCorteDataCompleto> corteDataCompletos = requestApi.getCortes().stream().map(corte -> {
+            Integer precoPorCorte = cortesResponseMap.get(corte.getId());
+            return TipoCorteDataCompleto.builder()
+                    .id(corte.getId())
+                    .tipoCorte(corte.getTipoCorte())
+                    .barbeiro(corte.getBarbeiro())
+                    .preco(precoPorCorte)
+                    .build();
+        }).toList();
 
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
         System.out.printf("Execution time: %.3f ms%n", duration / 1_000_000.0);
 
-        return result;
+
+        return MessageCompletaDTO.builder()
+                .cnpj(requestApi.getCnpj())
+                .nome(requestApi.getNome())
+                .cortes(corteDataCompletos)
+                .build();
+
     }
 
+
 }
-
-
-
